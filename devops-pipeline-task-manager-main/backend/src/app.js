0e5a8898619ebc06e3751d0e8d5a0a9aaffea41e
Abdellah <abdellah@gmail.com>
2026-02-22 00:41:24 +0100

    Refactor task routes into routes folder

--- devops-pipeline-task-manager-main/backend/src/app.js
+++ devops-pipeline-task-manager-main/backend/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const tasksRouter = require('./routes/tasks');
+app.use('/tasks', tasksRouter);
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running (Lab 1)" });
+  res.json({ message: "DevOps Task Manager API is running Lab 1" });
 });
 
 app.get('/tasks', (req, res) => {

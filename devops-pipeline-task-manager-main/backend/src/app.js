88be07469722d2a2dcc4475385b8fa579f755f9f
Abdellah <essabdellah77@gmail.com>
2026-03-31 19:39:52 +0200

    awal modification

--- devops-pipeline-task-manager-main/backend/src/app.js
+++ devops-pipeline-task-manager-main/backend/src/app.js
@@ -5,11 +5,7 @@ app.use(express.json());
 const tasksRouter = require('./routes/tasks');
 
 app.get('/', (req, res) => {
- feature/routes-refactor
-    res.json({ message: "Welcome from FEATURE branch" });
-
     res.json({ message: "Welcome from MAIN branch with refactored routes" });
-  main
 });
 
 app.use('/tasks', tasksRouter);
